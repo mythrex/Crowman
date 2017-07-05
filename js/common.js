7f1e9b84e1ea933c078fd2e13e80c2fa66fd2e24
mythrex <shivam.kaushik73@gmail.com>
2017-07-05 16:25:37 +0530

    Admin page progress

--- js/common.js
+++ js/common.js
@@ -13,4 +13,39 @@ function makeAlert(type,msg,time=3000){
     },100);
   },time);
 }
-//for removing an element after some time
+//Save data to localStorage
+function saveData(bankArr,dataObj){
+  localStorage.setItem("bank",JSON.stringify(bankArr));
+  localStorage.setItem("line-data",JSON.stringify(dataObj));
+}
+//fetch data from localStorage
+function fetchData(bankArr,dataObj){
+  let localData = localStorage.getItem("line-data");
+  let bankData = localStorage.getItem("bank");
+  if(localData){
+    dataObj = JSON.parse(localData);
+  }
+  if(bankData){
+    bankArr = JSON.parse(bankData);
+  }
+}
+
+//send and getNotification
+function sendNotification(text,notifArr){
+  notifArr.push(text);
+  localStorage.setItem('notification',notifArr);
+}
+function getNotification(notifArr){
+  let arr = localStorage.getItem('notification');
+  if(arr){
+    notifArr = arr;
+  }
+}
+//add people to  line
+function addPeopleToLine(jqueryObj,data){
+  for(let prop in data) {
+    jqueryObj.append(`<span class="fa fa-user" crowman-id="${prop}"
+    data-toggle="tooltip" data-placement="left"
+    title="cowman-id: ${prop}">`);
+  }
+}
